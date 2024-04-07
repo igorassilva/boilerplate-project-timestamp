aa0d1431e4bdbd4a0bb97acc328d546cc1e9783e
Igor Silva <igor.silva@labiexames.com.br>
2024-04-07 18:10:35 -0300

    feat: finish test

--- index.js
+++ index.js
@@ -20,8 +20,22 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  const date = req.params.date;
+
+    const unix = !date ? Date.now() : Math.floor(new Date(date).getTime() / 1000);
+    const utc = !date ? new Date().toUTCString() : new Date(date).toUTCString();
+    
+    if (!(unix instanceof Date)) {
+      res.json({
+        error: "Invalid Date"
+      }); 
+    }
+
+    res.json({
+      unix,
+      utc
+    });
 });
 
 
